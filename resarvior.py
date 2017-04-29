a23080017eb65617fa2a1bb25545e46ab1a4fb44
amatii <ahmad.razavi@gmail.com>
2017-04-29 11:58:25 -0700

    First commit

--- resarvior.py
+++ resarvior.py
@@ -6,8 +6,21 @@ import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('reservoir.csv',sep=',')
+
+
+
 print(df[df['id']=='ANT'])
 
+#exit()
+plt.figure()
+fig, ax = plt.subplots(figsize=(8,6))
 
+for Rid in ['ANT','ALM']:
+	temp = df[df['id']==Rid]
+	temp.plot(x='date',y='storage',ax=ax)
+
+plt.legend(loc='best')
+plt.savefig('./figures/'+'1.pdf', format='pdf')
+plt.close() 
 exit()
 
